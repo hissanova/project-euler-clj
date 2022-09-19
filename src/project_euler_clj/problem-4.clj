58f22a6d70295c741a941820ca8991d813555a1d
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2022-09-19 14:05:53 +0900

    problem 4

--- src/project_euler_clj/problem-4.clj
+++ src/project_euler_clj/problem-4.clj
@@ -1,6 +1,31 @@
 ;; A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 ;; Find the largest palindrome made from the product of two n-digit numbers.
 
+(defn n-digit-nums
+  [n]
+  (range (dec (int (Math/pow 10 n)))
+         (int (dec (Math/pow 10 (dec n))))
+         -1))
+(n-digit-nums 1)
+(defn perm-two-vecs
+  [vec1 vec2]
+  (for [x vec1
+        y vec2]
+    (* x y)))
 
-(map #(* %1 %2) (range 9 0 -1) (range 9 0 -1))
-(clojure.math)
+(perm-two-vecs (n-digit-nums 2) (n-digit-nums 2))
+
+(defn palindrome?
+  [n]
+  (= n (Integer/parseInt (apply str (reverse (str n))))))
+
+(int (apply str (reverse (str 19))))
+(Integer/parseInt "21")
+(palindrome? 101)
+
+(defn get-n-digits-palindrome
+  [n]
+  (filter palindrome? (perm-two-vecs (n-digit-nums n)
+                                     (n-digit-nums n))))
+
+(get-n-digits-palindrome 3)
